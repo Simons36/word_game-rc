70c141f2aad1ab1fa95fad08389fe4cabf8b3544
Simons36 <simaosilvassa@gmail.com>
2022-12-13 12:07:33 +0000

    added two functions

--- client/include/commands.h
+++ client/include/commands.h
@@ -14,5 +14,13 @@ void play_command(char * plid, char *letter);
 int plid_exists(char * plid);
 void play_place_letter(char letter, char * n_pos);
 void ignore_line();
+void play_wrong_letter(char letter);
+
+/**
+ * @brief Function used when server responds that client won the game;
+ * fills out remaining spots with letter, prints word
+ * and prints to the player winning message
+ */
+void play_win(char letter);
 
 #endif
